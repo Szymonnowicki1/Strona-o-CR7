4eeaa76fa2f76ca2b4f2e4877cf235421211662f
Szymonnowicki1 <92035422+Szymonnowicki1@users.noreply.github.com>
2024-09-12 00:36:25 +0200

    Update football.js

--- football.js
+++ football.js
@@ -1,66 +1,63 @@
 
 function handleClick(clubname) {
-
-    const ha2 = document.getElementById("ha2").style.display = "none";
-    const clubsContainer = document.getElementById("clubs").style.display = "none";
-    const clubInfo = document.getElementById("club-info").style.display = "block";
+    document.getElementById("ha2").style.display = "none";
+    document.getElementById("clubs").style.display = "none";
+    document.getElementById("club-info").style.display = "block";
     const clubImage = document.getElementById("club-image");
     const matchesElement = document.getElementById("matches");
     const asistsElement = document.getElementById("asists");
     const goalsElement = document.getElementById("goals");
     const trophiesElement = document.getElementById("trophies");
 
+    switch(clubname) {
+        case "sporting lizbona":
+            clubImage.innerHTML = '<img src="img/sporting_lizbona.webp" alt="" height="400" width="400">';
+            matchesElement.innerText = "Matches Played: 31"; 
+            asistsElement.innerText = "Assists: 6";
+            goalsElement.innerText = "Goals Scored: 5"; 
+            trophiesElement.innerText = "Trophies: 1";
+            break;
 
-    
-    if(clubname == "sporting lizbona"){
-        clubImage.innerHTML = '<img src="img/sporting_lizbona.webp" alt="" height="400" width="400">';
-        matchesElement.innerText = "Matches Played: 31"; 
-        asistsElement.innerHTML="Asists : 6";
-        goalsElement.innerText = "Goals Scored: 5"; 
-        trophiesElement.innerText = "Trophies: 1";
+        case "manchester united":
+            clubImage.innerHTML = '<img src="img/manchesterunited.png" alt="" height="400" width="400">';
+            matchesElement.innerText = "Matches Played: 346"; 
+            asistsElement.innerText = "Assists: 64";
+            goalsElement.innerText = "Goals Scored: 145"; 
+            trophiesElement.innerText = "Trophies: 9";
+            break;
 
+        case "real madryt":
+            clubImage.innerHTML = '<img src="img/Logo-real-madrid-club-clipart-PNG.png" alt="" height="400" width="400">';
+            matchesElement.innerText = "Matches Played: 438"; 
+            asistsElement.innerText = "Assists: 131";
+            goalsElement.innerText = "Goals Scored: 450"; 
+            trophiesElement.innerText = "Trophies: 15";
+            break;
+
+        case "juventus":
+            clubImage.innerHTML = '<img src="img/juventus-logo.png" alt="" height="400" width="400">';
+            matchesElement.innerText = "Matches Played: 134"; 
+            asistsElement.innerText = "Assists: 22";
+            goalsElement.innerText = "Goals Scored: 101"; 
+            trophiesElement.innerText = "Trophies: 5";
+            break;
+
+        case "all nassr":
+            clubImage.innerHTML = '<img src="img/al-nassr-fc-logo-1.png" alt="" height="400" width="400">';
+            matchesElement.innerText = "Matches Played: 68"; 
+            asistsElement.innerText = "Assists: 17";
+            goalsElement.innerText = "Goals Scored: 62"; 
+            trophiesElement.innerText = "Trophies: 1";
+            break;
+
+        case "portugal":
+            clubImage.innerHTML = '<img src="img/Portuguese_Football_Federation.svg.png" alt="" height="400" width="400">';
+            matchesElement.innerText = "Matches Played: 212"; 
+            asistsElement.innerText = "Assists: 45";
+            goalsElement.innerText = "Goals Scored: 130"; 
+            trophiesElement.innerText = "Trophies: 5";
+            break;
     }
-
-    else if(clubname == "manchester united"){
-        clubImage.innerHTML = '<img src="img/manchesterunited.png" alt="" height="400" width="400">';
-        matchesElement.innerText = "Matches Played: 346"; 
-        asistsElement.innerHTML="Asists : 64";
-        goalsElement.innerText = "Goals Scored: 145"; 
-        trophiesElement.innerText = "Trophies: 9";
-    }
-
-    else if(clubname == "real madryt"){
-        clubImage.innerHTML = '<img src="img/Logo-real-madrid-club-clipart-PNG.png" alt="" height="400" width="400">';
-        matchesElement.innerText = "Matches Played: 438"; 
-        asistsElement.innerHTML="Asists : 131";
-        goalsElement.innerText = "Goals Scored: 450"; 
-        trophiesElement.innerText = "Trophies: 15";
-    }
-
-    else if(clubname == "juventus"){
-        clubImage.innerHTML = '<img src="img/juventus-logo.png" alt="" height="400" width="400">';
-        matchesElement.innerText = "Matches Played: 134"; 
-        asistsElement.innerHTML="Asists : 22";
-        goalsElement.innerText = "Goals Scored: 101"; 
-        trophiesElement.innerText = "Trophies: 5";
-    }
-
-    else if(clubname == "all nassr"){
-        clubImage.innerHTML = '<img src="img/al-nassr-fc-logo-1.png" alt="" height="400" width="400">';
-        matchesElement.innerText = "Matches Played: 68"; 
-        asistsElement.innerHTML="Asists : 17";
-        goalsElement.innerText = "Goals Scored: 62"; 
-        trophiesElement.innerText = "Trophies: 1";
-    }
-
-    else if(clubname == "portugal"){
-        clubImage.innerHTML = '<img src="img/Portuguese_Football_Federation.svg.png" alt="" height="400" width="400">';
-        matchesElement.innerText = "Matches Played: 212"; 
-        asistsElement.innerHTML="Asists : 45";
-        goalsElement.innerText = "Goals Scored: 130"; 
-        trophiesElement.innerText = "Trophies: 5";
-    }
-
 }
 
 function goBack() {
